14e8f105308d82670ff3b2beb984a2f28cd917fb
apolog6 <119029589+apolog6@users.noreply.github.com>
2022-11-25 00:26:54 -0600

    Delete ProyectoFinal directory

--- ProyectoFinal/js/navbar.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.querySelector('.toggle')
-const navmenu = document.querySelector('.nav-menu')
-
-toggle.addEventListener('click', () => {
-
-    toggle.classList.toggle('rotate')
-    navmenu.classList.toggle('active')
-})
-
